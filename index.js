6b6ec57ea256e4b516d4ee7f6f1cb08fb4b28fed
ishankashyap <ikashyap@ualberta.ca>
2024-06-12 17:49:23 -0700

    etch-a-sketch dynamic

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const container = document.querySelector("#container");
 
-for(let i = 1; i<=256; i++){
-    const div = document.createElement("div");
-    container.appendChild(div);
-    div.addEventListener("mouseenter", (event) => {
-        div.classList.add("hovered");
-    });
+function makeGrid(number){
+    // Clear existing grid
+    container.innerHTML = '';
+
+    // let dimension = number * number;
+    for (let i = 1; i <= number; i++) {
+        const row = document.createElement("div");
+        container.appendChild(row);
+        for(let i = 1; i<= number; i++){
+            const column = document.createElement("div");
+            column.className = "squares";
+            row.appendChild(column);
+            column.addEventListener("mouseenter", () => {
+                column.classList.add("hovered");
+            });
+        }
+    }
 }
+
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+    let userInput = prompt("How many squares per side do you want?");
+    let gridSize = parseInt(userInput);
+    if (!isNaN(gridSize) && gridSize > 0 && gridSize <= 100) {
+        makeGrid(gridSize);
+    } else {
+        alert('Please enter a valid number between 1 and 100');
+    }
+});
